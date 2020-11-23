cb685567f720083cb38eba5b4d7daf4be9dfa40e
Nkwe Justice Mapoulo <nkwejustice1@gmail.com>
2020-11-23 23:31:42 +0200

    first commit

---
+++ SpringSecurity-SQL/src/main/java/com/example/demo/SpringSecuritySqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySqlApplication.class, args);
+	}
+
+}
